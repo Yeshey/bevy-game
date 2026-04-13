2ed412a37991fc0ad0c27c7527ca9c5d9e566bf1
Yeshey <yesheysangpo@gmail.com>
2026-04-13 02:08:46 +0100

    lets try again

---
+++ devenv-web.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    targets = [ "wasm32-unknown-unknown" ];  # only wasm, no android
+  };
+
+  packages = with pkgs; [
+    trunk
+    binaryen
+    wasm-pack
+    wasm-bindgen-cli
+    alsa-lib
+    udev
+    vulkan-loader
+    libxkbcommon
+    wayland
+    libx11
+    libxcursor
+    libxi
+    libxrandr
+    pkg-config
+    clang
+    mold
+  ];
+
+  env.LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+    vulkan-loader
+    wayland
+    libx11
+    libxcursor
+    libxi
+    libxrandr
+    libxkbcommon
+  ]);
+}
